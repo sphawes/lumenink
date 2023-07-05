9e9083ef7e8ff30514904d1ac03f4500322cfc2b
Stephen Hawes <sphawes@gmail.com>
2023-07-05 14:35:39 -0400

    fixes bad pin assignment in keyboard firmware

--- button_firmware/button_firmware.ino
+++ button_firmware/button_firmware.ino
@@ -1,21 +1,24 @@
 #include <Keyboard.h>
 
-int draw = 13;  //sends keyboard 1
+int draw = 11;  //sends keyboard 1
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(draw, INPUT);
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(!digitalRead(draw)){
+    delay(100);
+    Keyboard.write('d');
+    delay(500);
     while(!digitalRead(draw)){
       //do nothing, waiting for button to come up
     }
-    Keyboard.write('d');
-    delay(500);
   }
-
 }
+
+
